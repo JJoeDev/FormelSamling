45d4b28c2f2b78f5699e5226512bd26df8ebf934
jjoedev <jhknuds1@protonmail.com>
2024-04-27 00:31:43 +0200

    Away from SDL. To OpenGL we go!

--- src/main.cpp
+++ src/main.cpp
@@ -1,81 +1,92 @@
 #include <iostream>
-#include <SDL2/SDL.h>
 
 #include "../vendor/imgui/imgui.h"
-#include "../vendor/imgui/backends/imgui_impl_sdl2.h"
-#include "../vendor/imgui/backends/imgui_impl_sdlrenderer2.h"
+#include "../vendor/imgui/backends/imgui_impl_glfw.h"
+#include "../vendor/imgui/backends/imgui_impl_opengl3.h"
 
-#if !SDL_VERSION_ATLEAST(2,0,17)
-#error This backend requires SDL 2.0.17+ because of SDL_RenderGeometry() function
+#define GL_SILENCE_DEPRECATION
+
+#if defined (IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
 #endif
 
-int main(){
-    if(SDL_InitSubSystem(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) < 0){
-        std::cerr << "[ ERROR ] SDL2 failed initialization\n";
+#include <GLFW/glfw3.h>
+
+#if defined (_MSC_VER) && (_MSC_VER >= 1900) && !defined (IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
+
+static void glfw_error_callback(int error, const char* msg){
+    fprintf(stderr, "GLFW ERROR: %d: %s\n", error, msg);
+}
+
+int main(int, char**){
+    glfwSetErrorCallback(glfw_error_callback);
+    if(!glfwInit()){
+        std::cerr << "GLFW INIT FAILED\n";
         return -1;
     }
 
-#ifdef SDL_HINT_IME_SHOW_UI
-    SDL_SetHint(SDL_HINT_IME_SHOW_UI, "1");
+#if defined(__APPLE__)
+    const char* glsl_version = "#version 150";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#else
+    const char* glsl_version = "#version 330";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #endif
 
-    SDL_WindowFlags flags = (SDL_WindowFlags)(SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-    SDL_Window* window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 680, 480, flags);
+    GLFWwindow* window = glfwCreateWindow(680, 480, "Formel Samling", nullptr, nullptr);
     if(!window){
-        std::cerr << "[ ERROR ] SDL2 window error: " << SDL_GetError() << '\n';
+        std::cerr << "GLFW WINDOW CREATION FAILED\n";
         return -1;
     }
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1,SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
-    if(!renderer){
-        std::cerr << "[ ERROR ] SDL2 Render error: " << SDL_GetError() << '\n';
-        return -1;
-    }
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // enable VSync
 
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
-    //io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
-
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+    
     ImGui::StyleColorsDark();
 
-    ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
-    ImGui_ImplSDLRenderer2_Init(renderer);
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
 
-    SDL_SetRenderDrawColor(renderer, 20, 20, 20, 255);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
+    while(!glfwWindowShouldClose(window)){
+        glfwPollEvents();
 
-    bool running = true;
-    while(running){
-        SDL_Event e;
-        while(SDL_PollEvent(&e)){
-            if(e.type == SDL_QUIT) running = false;
-        }
-
-        ImGui_ImplSDLRenderer2_NewFrame();
-        ImGui_ImplSDL2_NewFrame();
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
         ImGui::ShowDemoWindow();
 
         ImGui::Render();
-        SDL_RenderSetScale(renderer, io.DisplayFramebufferScale.x, io.DisplayFramebufferScale.y);
-        SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
-        SDL_RenderClear(renderer);
-        ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData());
-        SDL_RenderPresent(renderer);
+        int displayW, displayH;
+        glfwGetFramebufferSize(window, &displayW, &displayH);
+        glViewport(0, 0, displayW, displayH);
+        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
     }
 
-    ImGui_ImplSDLRenderer2_Shutdown();
-    ImGui_ImplSDL2_Shutdown();
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
     ImGui::DestroyContext();
 
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
     return 0;
 }
