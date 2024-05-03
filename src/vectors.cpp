ac639955a6c34a2034831b69156967654acb422d
jjoedev <jhknuds1@protonmail.com>
2024-05-04 00:19:02 +0200

    Vector + Vector

--- src/vectors.cpp
+++ src/vectors.cpp
@@ -6,7 +6,26 @@ namespace visual{
     void Vectors::Draw() {
         ImGui::Begin("Hello Vectors", nullptr, ImGuiWindowFlags_NoCollapse);
 
-        if(ImGui::CollapsingHeader("Vector Basics")){
+        const char* vecMath[] = {"Vector Basics", "Vector +-"};
+        static int vecMathIdx = 0;
+        const char* vecPreviewVal = vecMath[vecMathIdx];
+
+        if(ImGui::BeginCombo("Vectors", vecPreviewVal, ImGuiComboFlags_HeightRegular)){
+            for(int i = 0; i < IM_ARRAYSIZE(vecMath); ++i){
+                const bool selected = (vecMathIdx == i);
+                if(ImGui::Selectable(vecMath[i], selected))
+                    vecMathIdx = i;
+            }
+
+            ImGui::EndCombo();
+        }
+
+        ImGui::Spacing();
+        ImGui::Separator();
+        ImGui::Spacing();
+
+        switch (vecMathIdx){
+        case 0:{
             static float v_x, v_y, v_len;
 
             ImGui::Text("Vector a = (%.3f, %.3f)", v_x, v_y);
@@ -23,7 +42,7 @@ namespace visual{
             
             DrawCanvas();
 
-            ImVec2 gridSpacing = ImVec2(50.0f, 50.0f);
+            ImVec2 gridSpacing = ImVec2{50.0f, 50.0f};
             for(float x = fmodf(m_canvasPos.x, gridSpacing.x); x < m_canvasSize.x; x += gridSpacing.x){
                 m_drawList->AddLine(ImVec2{m_canvasPos.x + x, m_canvasPos.y}, ImVec2{m_canvasPos.x + x, m_canvasPos.y + m_canvasSize.y}, IM_COL32(50, 50, 50, 255));
             }
@@ -32,12 +51,39 @@ namespace visual{
                 m_drawList->AddLine(ImVec2{m_canvasPos.x, m_canvasPos.y + y}, ImVec2{m_canvasPos.x + m_canvasSize.x, m_canvasPos.y + y}, IM_COL32(50, 50, 50, 255));
             }
 
-            ImVec2 vecEnd = ImVec2(m_canvasPos.x + v_x * 10, m_canvasPos.y + v_y * 10);
+            ImVec2 vecEnd = ImVec2{m_canvasPos.x + v_x * 10, m_canvasPos.y + v_y * 10};
             m_drawList->AddLine(m_canvasPos, vecEnd, IM_COL32(255, 255, 255, 255));
+            break;
         }
-        if(ImGui::CollapsingHeader("Vector addition")){
+        case 1:
             static float v_x, v_y, v_x2, v_y2;
+
+            ImGui::TextColored(ImColor(0, 255, 0, 255), "Vector a = (%.3f, %.3f)", v_x, v_y);
+            ImGui::TextColored(ImColor(0, 0, 255, 255), "Vector b = (%.3f, %.3f)", v_x2, v_y2);
+            ImGui::TextColored(ImColor(255, 0, 0, 255), "Vector c = (%.3f, %.3f)", v_x + v_x2, v_y + v_y2);
+
+            ImGui::Spacing();
+            ImGui::SeparatorText("Interactives");
+            ImGui::PushItemWidth(ImGui::GetContentRegionAvail().x * 0.48f);
+            ImGui::SliderFloat("ax", &v_x, 0, 30); ImGui::SameLine();
+            ImGui::SliderFloat("ay", &v_y, 0, 30);
+            ImGui::PopItemWidth();
+
+            ImGui::PushItemWidth(ImGui::GetContentRegionAvail().x * 0.48f);
+            ImGui::SliderFloat("ax2", &v_x2, 0, 30); ImGui::SameLine();
+            ImGui::SliderFloat("ay2", &v_y2, 0, 30);
+            ImGui::PopItemWidth();
+
             DrawCanvas();
+
+            // m_drawList->AddLine(ImVec2{m_canvasPos.x, m_canvasPos.y + v_y * 10}, ImVec2{m_canvasPos.x + v_x * 10, m_canvasPos.y + v_y * 10}, IM_COL32(50, 50, 50, 255));
+            // m_drawList->AddLine(ImVec2{m_canvasPos.x + v_x * 10, m_canvasPos.y + v_y * 10}, ImVec2{m_canvasPos.x + v_x * 10, m_canvasPos.y}, IM_COL32(50, 50, 50, 255));
+
+            ImVec2 v1End = ImVec2(m_canvasPos.x + v_x * 10, m_canvasPos.y + v_y * 10);
+            ImVec2 v2End = ImVec2(v1End.x + v_x2 * 10, v1End.y + v_y2 * 10);
+            m_drawList->AddLine(m_canvasPos, v1End, IM_COL32(0, 255, 0, 255));
+            m_drawList->AddLine(v1End, v2End, IM_COL32(0, 0, 255, 255));
+            m_drawList->AddLine(m_canvasPos, v2End, IM_COL32(255, 0, 0, 255));
         }
 
         ImGui::End();
